6a76410b5d0dc1b551f1189b62aefa37a0cf3fe1
milkrong <milkrong121@outlook.com>
2023-09-19 10:22:13 +0800

    feat: test code

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 -m venv ~/venv-metal
-source ~/venv-metal/bin/activate
+python3 -m venv ~/venv-pixcode
+source ~/venv-pixcode/bin/activate
 python -m pip install -U pip
 python -m pip install Keras tensorflow opencv-python numpy
